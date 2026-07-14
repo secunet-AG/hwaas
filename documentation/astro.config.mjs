7ae122c77139ab611ef96ed77f63772330405c16
Luke <luke.weston@cyberus-technology.de>
2026-07-14 13:24:06 +0200

    fix: use astro component for open api docs

--- documentation/astro.config.mjs
+++ documentation/astro.config.mjs
@@ -1,17 +1,4 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import scalarAstro from "@scalar/astro";
-
 // https://astro.build/config
-export default defineConfig({
-  integrations: [
-    scalarAstro({
-      spec: {
-        url: "/openapi.json",
-      },
-      configuration: {
-        title: "HWaaS API",
-      },
-    }),
-  ],
-});
+export default defineConfig({});
