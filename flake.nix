1bef50339fb2b9a6dd33045b3b3929319f6181fb
Markus Napierkowski <markus.napierkowski@cyberus-technology.de>
2026-07-15 11:00:41 +0200

    nix: bump all flake inputs, remove unused inputs, clean up flake.nix

--- flake.nix
+++ flake.nix
@@ -5,19 +5,9 @@
 {
   description = "HWaaS";
 
-  nixConfig = {
-    extra-substituters = [
-      "https://microvm.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "microvm.cachix.org-1:oXnBc6hRE3eX5rSYdRyMYXnfzcCxC7yKPTbZXALsqys="
-    ];
-  };
-
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    mission-control.url = "github:Platonic-Systems/mission-control";
     git-hooks-nix = {
       url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -26,10 +16,7 @@
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-root.url = "github:srid/flake-root";
     crane.url = "github:ipetkov/crane";
-    nixos-hardware.url = "github:nixos/nixos-hardware/master";
-    microvm.url = "github:astro/microvm.nix";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -38,21 +25,14 @@
       url = "github:edolstra/flake-compat";
       flake = false;
     };
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
     inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
-        inputs.mission-control.flakeModule
-        inputs.flake-root.flakeModule
         inputs.git-hooks-nix.flakeModule
         inputs.treefmt-nix.flakeModule
-        #inputs.colmena-flake.flakeModules.default
         ./nix
       ];
 
