576631a73384a6ed95090703d8ad869373f2d25c
lukeweston1234 <lukeweston1234@gmail.com>
2026-07-16 15:26:26 +0200

    feat: more styling

--- documentation/astro.config.mjs
+++ documentation/astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
+  integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()],
   },
