e0ae44fa9c64d42a861d8614defad87d6c431d41
Lucas Grosche <lucas.grosche@cyberus-technology.de>
2026-07-20 10:38:10 +0200

    user-tooling: add license check to Python code

--- user-tooling/packages/benchmark-data-collector/default.nix
+++ user-tooling/packages/benchmark-data-collector/default.nix
@@ -3,6 +3,54 @@
 # SPDX-License-Identifier: Apache-2.0
 
 { pkgs }:
+
+let
+  py = pkgs.python3Packages;
+
+  pip-licenses = py.buildPythonApplication rec {
+    pname = "pip-licenses";
+    version = "5.5.0";
+    pyproject = true;
+
+    src = pkgs.fetchPypi {
+      pname = "pip_licenses";
+      inherit version;
+      hash = "sha256-JHPnr9AqDCFGB1j3D9K7OzwIDFFQcT3TO6qUk9wVY6U=";
+    };
+
+    build-system = [
+      py.setuptools
+      py.setuptools-scm
+    ];
+
+    dependencies = [
+      py.prettytable
+    ];
+  };
+
+  generallyAllowedLicenses = [
+    "MIT"
+    "MIT License"
+    "Apache-2.0"
+    "BSD-2-Clause"
+    "BSD-3-Clause"
+    "Apache-2.0 OR BSD-2-Clause"
+    "PSF-2.0"
+  ];
+  generallyAllowedLicensesString = builtins.concatStringsSep ";" generallyAllowedLicenses;
+
+  # These licenses are not generally allowed, but the packages were manually verified.
+  # See NOTICE file in repository root for details.
+  licenseExceptions = [
+    {
+      package = "pathspec";
+      license = "Mozilla Public License 2.0 (MPL 2.0)";
+    }
+  ];
+  licenseExceptionsPackagesString = pkgs.lib.strings.concatMapStringsSep " " (p: p.package) licenseExceptions;
+  licenseExceptionsLicensesString = pkgs.lib.strings.concatMapStringsSep ";" (p: p.license) licenseExceptions;
+in
+
 pkgs.python3Packages.buildPythonPackage {
   name = "benchmark-data-collector";
   src = ./.;
@@ -17,7 +65,7 @@ pkgs.python3Packages.buildPythonPackage {
   pyproject = true;
 
   doCheck = true;
-  nativeCheckInputs = with pkgs.python3Packages; [ black mypy pkgs.ruff pytest ];
+  nativeCheckInputs = with pkgs.python3Packages; [ black mypy pkgs.ruff pytest pip-licenses ];
   checkPhase = ''
     echo "## run mypy"
     mypy benchmark_data_collector
@@ -27,5 +75,19 @@ pkgs.python3Packages.buildPythonPackage {
     black --check --diff .
     echo "## run pytest"
     pytest
+    echo "## check generally allowed Python dependency licenses"
+    pip-licenses \
+      --from=mixed \
+      --format=plain \
+      --with-urls \
+      --ignore-packages ${licenseExceptionsPackagesString} \
+      --allow-only='${generallyAllowedLicensesString}'
+    echo "## check approved licensing exceptions"
+    pip-licenses \
+      --from=mixed \
+      --format=plain \
+      --with-urls \
+      --packages ${licenseExceptionsPackagesString} \
+      --allow-only='${licenseExceptionsLicensesString}'
   '';
 }
--- user-tooling/packages/hwaas-timer/default.nix
+++ user-tooling/packages/hwaas-timer/default.nix
@@ -2,13 +2,62 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-{ buildPythonPackage
+{ pkgs
+, buildPythonPackage
 , black
 , mypy
 , pytest
 , ruff
 , setuptools
 }:
+let
+  py = pkgs.python3Packages;
+
+  pip-licenses = py.buildPythonApplication rec {
+    pname = "pip-licenses";
+    version = "5.5.0";
+    pyproject = true;
+
+    src = pkgs.fetchPypi {
+      pname = "pip_licenses";
+      inherit version;
+      hash = "sha256-JHPnr9AqDCFGB1j3D9K7OzwIDFFQcT3TO6qUk9wVY6U=";
+    };
+
+    build-system = [
+      py.setuptools
+      py.setuptools-scm
+    ];
+
+    dependencies = [
+      py.prettytable
+    ];
+  };
+
+  generallyAllowedLicenses = [
+    "MIT"
+    "MIT License"
+    "Apache-2.0"
+    "Apache Software License"
+    "BSD-2-Clause"
+    "BSD-3-Clause"
+    "BSD License"
+    "Apache-2.0 OR BSD-2-Clause"
+    "PSF-2.0"
+  ];
+  generallyAllowedLicensesString = builtins.concatStringsSep ";" generallyAllowedLicenses;
+
+  # These licenses are not generally allowed, but the packages were manually verified.
+  # See NOTICE file in repository root for details.
+  licenseExceptions = [
+    {
+      package = "pathspec";
+      license = "Mozilla Public License 2.0 (MPL 2.0)";
+    }
+  ];
+  licenseExceptionsPackagesString = pkgs.lib.strings.concatMapStringsSep " " (p: p.package) licenseExceptions;
+  licenseExceptionsLicensesString = pkgs.lib.strings.concatMapStringsSep ";" (p: p.license) licenseExceptions;
+in
 buildPythonPackage {
   name = "hwaas-timer";
   src = ./.;
@@ -20,7 +69,7 @@ buildPythonPackage {
   pyproject = true;
 
   doCheck = true;
-  nativeCheckInputs = [ black mypy ruff pytest ];
+  nativeCheckInputs = [ black mypy ruff pytest pip-licenses ];
   checkPhase = ''
     echo "## run mypy"
     mypy hwaas_timer
@@ -30,5 +79,19 @@ buildPythonPackage {
     black --check --diff .
     echo "## run pytest"
     pytest
+    echo "## check generally allowed Python dependency licenses"
+    pip-licenses \
+      --from=mixed \
+      --format=plain \
+      --with-urls \
+      --ignore-packages ${licenseExceptionsPackagesString} \
+      --allow-only='${generallyAllowedLicensesString}'
+    echo "## check approved licensing exceptions"
+    pip-licenses \
+      --from=mixed \
+      --format=plain \
+      --with-urls \
+      --packages ${licenseExceptionsPackagesString} \
+      --allow-only='${licenseExceptionsLicensesString}'
   '';
 }
