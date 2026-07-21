80b79046fe2b638bdf37b00e45d34465715c0ecb
lukeweston1234 <lukeweston1234@gmail.com>
2026-07-21 13:25:58 +0200

    fix: path for initial redirect based on prod or dev

--- documentation/astro.config.mjs
+++ documentation/astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
+
+const isProd = process.env.NODE_ENV === "production";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://secunet-ag.github.io/hwaas/",
+  site: isProd ? "https://secunet-ag.github.io" : "http://localhost:4321",
+  base: isProd ? "/hwaas/" : "/",
   integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()],
