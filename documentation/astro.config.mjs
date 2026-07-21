99f28f0be20a4494eccbf86adabf6f9847218969
lukeweston1234 <lukeweston1234@gmail.com>
2026-07-21 12:56:52 +0200

    fix: update site and base for astro config

--- documentation/astro.config.mjs
+++ documentation/astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
+  site: "https://secunet-ag.github.io",
+  base: "/hwaas/",
   integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()],
