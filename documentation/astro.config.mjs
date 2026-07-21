67f4912ce6f0ab592aa4740b286ee0b265999815
lukeweston1234 <lukeweston1234@gmail.com>
2026-07-21 13:17:26 +0200

    fix: path for routing

--- documentation/astro.config.mjs
+++ documentation/astro.config.mjs
@@ -4,17 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
-  site: "https://secunet-ag.github.io",
-  base: "/hwaas/",
+  site: "https://secunet-ag.github.io/hwaas/",
   integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()],
     server: {
       allowedHosts: true,
     },
-  },
-  redirects: {
-    "/": "/docs/getting-started",
   },
   markdown: {
     shikiConfig: {
