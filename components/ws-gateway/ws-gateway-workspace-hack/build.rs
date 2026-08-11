2b46bb20f1d4f4ff57baeefc6bb33bfb241c5f3f
Andreas Hartmann <andreas.hartmann@cyberus-technology.de>
2026-08-11 15:52:39 +0200

    refactor: Move `ws-gateway` into shared workspace
    and factor `ws-proxy-client` into a standalone submodule in the process.
    It wasn't previously integrated into the `ws-gateway` workspace anyhow,
    but it's apparently also not obsolete as at least some NixOS tests
    depend on this utility. Hence, it's not a standalone workspace member.

--- components/ws-gateway/ws-gateway-workspace-hack/build.rs
+++
@@ -1,6 +0,0 @@
-// SPDX-FileCopyrightText: Copyright 2026 secunet Security Networks AG <https://www.secunet.com>
-//
-// SPDX-License-Identifier: Apache-2.0
-
-// A build script is required for cargo to consider build dependencies.
-fn main() {}
