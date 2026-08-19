e7e2b6f3198445eab1e8d84db5f72c5e840b3068
Lucas Grosche <lucas.grosche@cyberus-technology.de>
2026-08-19 16:05:43 +0200

    refactor: Adapt crane to workspace changes

---
+++ components/workspace-hack/src/lib.rs
@@ -0,0 +1,6 @@
+// SPDX-FileCopyrightText: Copyright 2026 secunet Security Networks AG <https://www.secunet.com>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+// This is a stub lib.rs.
+// Workspace dependency unification crate managed by cargo-hakari.
